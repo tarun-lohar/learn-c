069cceb524c51e3a1563e1caf3f5f020f43522b8
tarun-lohar <ramanlohar12@gmail.com>
2026-09-23 16:18:38 +0000

    first c code

---
+++ learn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float sum(float a, float b) { return a * b; }
+
+int main()
+{
+    int i = 0;
+    while ("kfyjg")
+    {
+
+        if (i > 10)
+        {
+            break;
+        }
+
+        printf("%d\n", i);
+        i = i + 2;
+    }
+}
